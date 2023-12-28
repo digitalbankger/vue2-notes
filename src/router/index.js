23aec480af519bc1d6dab9edb7f514871a3c306a
digitalbankger <bkmzen.bkmzen@mail.ru>
2023-12-28 23:38:52 +0300

    router

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import PublicLayout from '@/layouts/PublicLayout.vue';
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
 import GeneralPage from '@/pages/public/GeneralPage.vue';
 import UserDashboard from '@/pages/user/UserDashboard.vue';
+import { createWebHistory } from 'vue-router';
 
 Vue.use(Router);
 const routerHistory = createWebHistory('/vue2-notes/');
