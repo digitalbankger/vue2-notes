951333537660cbb6150529e23c894ed767844671
digitalbankger <bkmzen.bkmzen@mail.ru>
2023-12-29 09:44:58 +0300

    Cpm

--- src/router/index.js
+++ src/router/index.js
@@ -4,35 +4,33 @@ import PublicLayout from '@/layouts/PublicLayout.vue';
 import DashboardLayout from '@/layouts/DashboardLayout.vue';
 import GeneralPage from '@/pages/public/GeneralPage.vue';
 import UserDashboard from '@/pages/user/UserDashboard.vue';
-import { createWebHistory } from 'vue-router';
 
 Vue.use(Router);
-const routerHistory = createWebHistory('/vue2-notes');
 
 const router = new Router({
-  history: routerHistory,
-  routes: [
-    {
-      path: '/',
-      component: PublicLayout,
-      children: [
-        {
-          path: '',
-          component: GeneralPage,
-        },
-      ],
-    },
-    {
-      path: '/dashboard',
-      component: DashboardLayout,
-      children: [
-        {
-          path: '',
-          component: UserDashboard,
-        },
-      ],
-    },
-  ],
+    mode: 'history',
+    routes: [
+      {
+        path: '/',
+        component: PublicLayout,
+        children: [
+          {
+            path: '',
+            component: GeneralPage,
+          },
+        ],
+      },
+      {
+        path: '/dashboard',
+        component: DashboardLayout,
+        children: [
+          {
+            path: '',
+            component: UserDashboard,
+          },
+        ],
+      },
+    ],
 });
 
 export default router;
--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
 
 module.exports = defineConfig({
-  publicPath: '/vue2-notes',
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
